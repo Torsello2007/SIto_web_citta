8c1632e1a05ab6bcd4d7ab0c37e14b62c8bdac49
Jun Hu <hu.jun@iisgalvanimi.edu.it>
2024-11-14 16:32:36 +0000

    sistemazione homepage, aggiunto 'luoghi'

--- app.py
+++ app.py
@@ -9,6 +9,11 @@ def homepage():
 @app.route('/storia')
 def storia():
     return render_template('storia.html')
-    
+
+@app.route('/luoghi')
+def luoghi():
+    return render_template('luoghi.html')
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port = 3245, debug=True)
