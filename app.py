72972fafd296d370e66f96f8e467f8403f4423f0
Jun Hu <hu.jun@iisgalvanimi.edu.it>
2024-11-14 16:04:14 +0000

    aggiunto storia, testato spostamento pagina in pagina

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return render_template('homepage.html')
+
+@app.route('/storia')
+def storia():
+    return render_template('storia.html')
+    
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port = 3245, debug=True)
